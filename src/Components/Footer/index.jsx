ca72a01754c6deb97c2094c8b7688b44e4238f09
Valdir Junior <slipkinoot@hotmail.com>
2022-09-27 14:26:09 -0300

    fix. arrumando tela

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -2,11 +2,11 @@ import {FaFacebook,FaInstagram,FaGithub,FaCopyright} from 'react-icons/fa'
 
 export const Footer = () => {
     return(
-        <div className="relative mx-40 flex text-white justify-between flex-shrink-0">
+        <div className="relative mx-40 flex text-white justify-between mb-4 flex-shrink-0">
             <div className='flex items-center gap-3'>
-                <FaFacebook />
-                <FaInstagram />
-                <FaGithub />
+                <FaFacebook size={30}/>
+                <FaInstagram size={30}/>
+                <FaGithub size={30}/>
             </div>
             <div className='flex items-center'>
                 <FaCopyright/> 
--- src/Home/Index.jsx
+++ src/Home/Index.jsx
@@ -11,12 +11,10 @@ function App() {
         <div className="flex-initial">
           <span className="bg-textGradient text-7xl text-clip text-transparent bg-clip-text my-10">Em Breve</span>
           <p className="text-gray-400 text-xl">Mussum Ipsum, cacilds vidis litro abertis. Mé faiz elementum girarzis, 
-            nisi eros vermeio.Nec orci ornare consequat. Praesent lacinia ultrices consectetur. 
-            Sed non ipsum felis.In elementis mé pra quem é amistosis quis leo.Atirei o
-            pau no gatis, per gatis num morreus.</p>
+            nisi eros vermeio.Nec orci ornare consequat. Praesent lacinia ultrices consectetur.</p>
         </div>
         <div className="flex items-center justify-center">
-          <img src={frontImage} width={600} alt="" />
+          <img src={frontImage} width={400} alt="" />
         </div>
       </div>
       <Footer />
