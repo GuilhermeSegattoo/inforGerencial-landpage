68e871a3e27bfec7f20dc53d6539ceb426082c32
arthur <arthuroliveira909.av@gmail.com>
2022-09-28 11:04:07 -0300

    fix. cabeça

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -2,7 +2,7 @@
 export const Header = () => {
     return (
         <div className="bg-black flex items-center py-5 px-10 md:px-16 lg:px-28">
-            <img src="assets/logo.png" className="h-10 " alt="" />
+            <img src="assets/logo.png" className="h-10œ" alt="" />
         </div>
     )
 }
