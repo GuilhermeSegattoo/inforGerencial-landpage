0ecbf92ddf22abad3a1a860714870c9344a8fa97
Valdir Junior <slipkinoot@hotmail.com>
2022-09-28 16:53:31 -0300

    refact. responsivo

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,8 @@ const Home: NextPage = () => {
       <main className='relative min-h-screen'>
         <div className="flex flex-col relative min-h-screen h-full">
           <Header />
-          <div style={{height:'calc(100vh - 80px)'}}>
             <div className='lg:px-32 lg:py-10 lg:h-5/6 block'>
-              <div className="relative grid px-16 py-5 lg:grid-cols-2 lg:px-24 lg:py-24 items-center text-white justify-between
+              <div className=" max-w-max relative grid px-16 py-5 lg:grid-cols-2 lg:px-24 lg:py-24 items-center text-white justify-between
                 bg-zinc-400/5 rounded-lg h-full ">
                 <div className="order-last md:order-none">
                   <h1 className="bg-textGradient text-5xl md:text-6xl lg:text-7xl text-clip 
@@ -26,8 +25,7 @@ const Home: NextPage = () => {
                   text-transparent bg-clip-text my-5">
                     Em Breve
                   </h1>
-                  <p className="text-white text-xl text-center md:text-justify">Mussum Ipsum, cacilds vidis litro abertis. Mé faiz elementum girarzis, 
-                    nisi eros vermeio.Nec orci ornare consequat. Praesent lacinia ultrices consectetur.</p>
+                  <p className="text-white text-xl text-center md:text-justify">A goDev nasceu do espirito e da vontade de empreender dos estudantes de computação da Universidade Federal Rural do Semiárido, e de levar vocês em uma jornada pensada para conduzir suas mentes em um processo de construção visual e tecnológica, dentro do espectro do desenvolvimento digital. Os olhos estão abrindo, e a você não vai querer perder esse nascimento.</p>
                 </div>
                 <div className="flex items-center justify-center">
                   {/*<img src="./assets/fontImage.png" width={400} alt=""/>*/}
@@ -36,7 +34,6 @@ const Home: NextPage = () => {
               </div>
             </div>
             <Footer />
-          </div>
         </div>
       </main>
 
