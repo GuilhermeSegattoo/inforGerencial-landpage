c7464bbd34a82f734cb8abfe09c2a2d0b91b2fd9
Valdir Junior <slipkinoot@hotmail.com>
2022-10-14 11:09:33 -0300

    refact. Ajustes

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,4 +1,5 @@
-import {FaFacebook,FaInstagram,FaGithub,FaCopyright} from 'react-icons/fa'
+import Link from 'next/link'
+import {FaFacebook,FaInstagram,FaGithub,FaCopyright, FaLinkedin} from 'react-icons/fa'
 
 export const Footer = () => {
     return(
@@ -7,11 +8,18 @@ export const Footer = () => {
         px-10 md:px-16 lg:px-32
         text-white justify-between flex-shrink-0 ">
             <div className='flex items-center gap-3'>
-                <FaFacebook className="text-2xl md:text-3xl text-zinc-400"/>
-                <FaInstagram className="text-2xl md:text-3xl text-zinc-400"/>
-                <FaGithub className="text-2xl md:text-3xl text-zinc-400"/>
+                <a href="https://www.facebook.com/profile.php?id=100085246966884" target="blank">
+                    <FaFacebook className="text-2xl md:text-3xl text-zinc-400 hover:opacity-50 cursor-pointer"/>
+                </a>
+                <a href="https://www.instagram.com/godevofc/" target="blank">
+                    <FaInstagram className="text-2xl md:text-3xl text-zinc-400 hover:opacity-50 cursor-pointer"/>
+                </a>
+                <a href="">
+                    <FaLinkedin className="text-2xl md:text-3xl text-zinc-400 hover:opacity-50 cursor-pointer"/>
+                </a>
             </div>
             <div className='flex items-center gap-2 text-zinc-400'>
+                <span>GoDev - 2022 - </span>
                 <FaCopyright className='text-zinc-400'/> 
                 <span className='text-zinc-400'>Todos os direitos reservados</span> 
             </div>
