6e136223466dca30bcdc1fd0326fa0fe71219a48
wenderenzo <wenderenzo2014@outlook.com>
2022-12-06 21:36:43 -0300

    fix.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,12 +2,12 @@ import { Button } from "../button"
 
 export const Header = () => {
     return (
-        <nav className="bg-black flex items-center justify-center cel:px-5 px-14 w-full z-[99] shadow">
+        <nav className="bg-black flex items-center justify-center cel:px-5 px-14 w-full z-[99] shadow h-16">
             <section className="container items-center flex cel:justify-center tab:justify-between lap:justify-between">
                 <div className="cel:flex cel:justify-center cel:items-center cel:text-center lap:">
                     <img src="assets/logo.png" className="h-10" alt="" />
                 </div>
-                <ul className="flex xl:gap-16 tab:gap-4 cel:hidden tab:inline-flex lap:inline-flex xl:inline-flex  ">
+                <ul className="flex xl:gap-16 tab:gap-4 cel:hidden tab:inline-flex lap:inline-flex xl:inline-flex">
                     <a href="">
                         <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline p-8">
                             <span>Início</span>
