e5b80f94bb82bb2114bda7567a840a44c9384648
Mateus Lanuce <mateuslanuce@gmail.com>
2023-01-21 22:21:18 -0300

    refactor: :wheelchair: melhorada a acessibilidade, e feito o SEO do site
    adição de varias meta tags necessarias, ajuste de imagens e adição de aria-labels e roles

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -10,31 +10,31 @@ export const Header = () => {
     
     return (
         <nav className="bg-black flex items-center justify-center cel:px-5 px-14 w-full z-[99] shadow h-16 lg:h-[85px] fixed">
-            <section className="container items-center flex cel:justify-center tab:justify-between lap:justify-between">
+            <section role="menubar" aria-label="navigation menu bar"  className="container items-center flex cel:justify-center tab:justify-between lap:justify-between">
                 <div className="w-28 cel:flex cel:justify-center cel:items-center cel:text-center lap:">
-                    <img src="assets/logo.svg"  alt="" />
+                    <img src="assets/logo.svg"  alt="logo" />
                 </div>
                 <ul className="flex xl:gap-16 tab:gap-4 cel:hidden tab:inline-flex lap:inline-flex xl:inline-flex justify-center items-center">
-                    <a href="#inicio">
-                        <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline p-8 sm:p-5 lg:p-8">
+                    <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline">
+                        <a href="#inicio" className="p-8 sm:p-5 lg:p-8 inline-block">
                             <span>Início</span>
-                        </li>
-                    </a>
-                    <a href="#sobre">
-                        <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline p-8 sm:p-5 lg:p-8">
+                        </a>
+                    </li>
+                    <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline">
+                        <a href="#sobre" className="p-8 sm:p-5 lg:p-8 inline-block">
                             <span>Sobre</span>
-                        </li>
-                    </a>
-                    <a href="#solucao">
-                        <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline p-8 sm:p-5 lg:p-8">
-                            Nossa solução
-                        </li>
-                    </a>
-                    <a>
-                        <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline p-8 sm:p-5 lg:p-8">
+                        </a>
+                    </li>
+                    <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline">
+                        <a href="#solucao" className="p-8 sm:p-5 lg:p-8 inline-block">
+                            <span>Nossa solução</span>
+                        </a>
+                    </li>
+                    <li className="cursor-pointer text-slate-100 text-sm font-normal hover:text-white link-underline">
+                        <a href="#contato" className="p-8 sm:p-5 lg:p-8 inline-block">
                             <span>Tecnologias</span>
-                        </li>
-                    </a>
+                        </a>
+                    </li>
                 </ul>
                 <div className="cel:hidden tab:inline-block lap:inline-block xl:inline-block">
                     <a href="#contato">
@@ -42,7 +42,7 @@ export const Header = () => {
                     </a>
                 </div>
             </section>
-            <section className="block cel:inline-block tab:hidden lap:hidden xl:hidden">
+            <section role="menu" aria-label="navigation menu button" className="block cel:inline-block tab:hidden lap:hidden xl:hidden">
                 <div className="text-[#D30490] cursor-pointer" onClick={() => {setIsMenuOpen(!isMenuOpen)}}>
                     <GiHamburgerMenu size={30} />
                 </div>
