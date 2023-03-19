62e0d5b0781f1f15261ed454c474c9176f18daaf
Inhegaz <vinipegacion@gmail.com>
2023-03-18 22:29:03 -0300

    new modifies responsivity

--- components/home/inicio/index.tsx
+++ components/home/inicio/index.tsx
@@ -2,15 +2,16 @@ import { Button } from "../../utils/button"
 
 export const Inicio = () => {
     return (
-        <section id="inicio" role="contentinfo" aria-labelledby='inicio' className='flex pt-16 pb-16 mx-6 my-10 w-screen items-center justify-around lg:h-screen'>            
+        <section id="inicio" role="contentinfo" aria-labelledby='inicio' className='flex py-8 mx-10 my-10 w-screen items-center justify-around lg:h-screen'>            
           
-            <div className='grid grid-cols-1 text-white tab:text-start cel:text-center items-center flex-wrap justify-center z-50'>
-                <div className='cel:text-4xl cel:mt-14 tab:text-4xl tab:w-[300px] lap:w-[600px] lap:text-5xl desk:text-7xl  bg-textGradient bg-clip-text text-transparent font-bold mt-28'>
+            <div className='grid grid-cols-1 text-white tab:text-start cel:pb-0 cel:mb-0 cel:text-center items-center flex-wrap justify-center z-50'>
+                <div className='cel:text-4xl cel:mt-10 tab:text-4xl tab:w-[300px] lap:w-[600px] lap:text-5xl desk:text-7xl  bg-textGradient bg-clip-text text-transparent font-bold mt-28'>
                     <h1>Acelere seu <br /> desenvolvimento</h1>
                 </div>
                 
                 <p className='cel:text-lg tab:text-lg lap:text-xl desk:text-2xl mt-4 text-slate-100'>
-                    Construa a tecnologia do seu negócio com rapidez, qualidade e sem burocracia.
+                    Construa a tecnologia do seu negócio com rapidez, qualidade <br/>
+                    e sem burocracia.
                 </p>
                 <div className='mt-8 flex cel:justify-center tab:justify-start'>
                     <Button onClick={() => { }} appendClassName='w-[300px] h-10' >
