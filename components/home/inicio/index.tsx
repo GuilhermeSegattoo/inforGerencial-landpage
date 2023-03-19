dea25ef4ee70142f6790e19ab48444eb69f1455f
Inhegaz <vinipegacion@gmail.com>
2023-03-18 21:45:16 -0300

    new modifications in task #19

--- components/home/inicio/index.tsx
+++ components/home/inicio/index.tsx
@@ -5,7 +5,7 @@ export const Inicio = () => {
         <section id="inicio" role="contentinfo" aria-labelledby='inicio' className='flex pt-16 pb-16 mx-6 my-10 w-screen items-center justify-around lg:h-screen'>            
           
             <div className='grid grid-cols-1 text-white tab:text-start cel:text-center items-center flex-wrap justify-center z-50'>
-                <div className='cel:text-4xl cel:mt-14 tab:text-4xl lap:text-5xl desk:text-7xl bg-textGradient bg-clip-text text-transparent font-bold mt-28'>
+                <div className='cel:text-4xl cel:mt-14 tab:text-4xl tab:w-[300px] lap:w-[600px] lap:text-5xl desk:text-7xl  bg-textGradient bg-clip-text text-transparent font-bold mt-28'>
                     <h1>Acelere seu <br /> desenvolvimento</h1>
                 </div>
                 
@@ -22,8 +22,7 @@ export const Inicio = () => {
                 <div className='relative h-[350px] w-[400px]'>
                     <img className='absolute bottom-5 left-0 z-50 floating w-44 ' src='./assets/rocket/rocketWithFire.svg' alt='foguete' />
                     <img className='absolute top-10 left-5 z-30 spin' src='./assets/rocket/planets.svg' alt='planeta' />
-                    <img className='absolute top-0 left-10 z-20 pulse ' style={{ animationDelay: "5s" }} src='./assets/rocket/starts1.svg' alt='estrela' />
-                    <img className='absolute top-0 left-0 z-20 pulse ' style={{ animationDelay: "5s" }} src='./assets/rocket/starts2.svg' alt='estrela' />
+                    <img className='absolute top-0 left-0 z-20 pulse ' style={{ animationDelay: "5s" }} src='./assets/rocket/starts1.svg' alt='estrela' />
                 </div>
             </div>
         </section>
