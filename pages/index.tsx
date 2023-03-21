fcee2104341d043056260320235cb3b217d00513
Rudinilly-ti <rudinilly-ti@outolook.com>
2023-03-21 10:23:14 -0300

    Fix.Task#18/#20

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { Contato } from '../components/home/contato';
 import { Inicio } from '../components/home/inicio';
+import { Mercado } from '../components/home/mercado';
+import { PassoAPasso } from '../components/home/passo_a_passo';
+import { QuemSomos } from '../components/home/quem_somos';
 import { Sobre } from '../components/home/sobre';
 import { Solucao } from '../components/home/solucao';
-import { Contato } from '../components/home/contato';
 import { Template } from '../components/utils/template';
-import { Mercado } from '../components/home/mercado';
 
 const Home: NextPage = () => {
     return (
         <Template>
             {/* Secctions da landpage */}
             <Inicio/>
+            <QuemSomos/>
+            <PassoAPasso/>
             <Sobre/>
             <Mercado/>
             <Solucao/>
