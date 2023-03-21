eb1596c9784a198d53298f58468b965664efe442
Inhegaz <vinipegacion@gmail.com>
2023-03-20 21:39:23 -0300

    fix.layout

--- components/home/inicio/index.tsx
+++ components/home/inicio/index.tsx
@@ -2,7 +2,7 @@ import { Button } from "../../utils/button"
 
 export const Inicio = () => {
     return (
-        <section id="inicio" role="contentinfo" aria-labelledby='inicio' className='flex py-8 mx-10 my-10 w-screen items-center justify-around lg:h-screen'>            
+        <section id="inicio" role="contentinfo" aria-labelledby='inicio' className='flex py-8 lap:mx-10 desk:mx-6 my-10 w-screen items-center justify-around lg:h-screen'>            
           
             <div className='grid grid-cols-1 text-white tab:text-start cel:pb-0 cel:mb-0 cel:text-center items-center flex-wrap justify-center z-50'>
                 <div className='cel:text-4xl cel:mt-10 tab:text-4xl tab:w-[300px] lap:w-[600px] lap:text-5xl desk:text-7xl  bg-textGradient bg-clip-text text-transparent font-bold mt-28'>
@@ -19,8 +19,8 @@ export const Inicio = () => {
                     </Button>
                 </div>
             </div>
-            <div className='flex cel:hidden tab:hidden lap:inline-block xl:inline-block lap:w-auto'>
-                <div className='relative h-[350px] w-[400px]'>
+            <div className='flex cel:hidden tab:hidden lap:inline-block xl:inline-block lap:auto'>
+                <div className='relative h-[350px] w-[400px] xl:ml-80'>
                     <img className='absolute bottom-5 left-0 z-50 floating w-44 ' src='./assets/rocket/rocketWithFire.svg' alt='foguete' />
                     <img className='absolute top-10 left-5 z-30 spin' src='./assets/rocket/planets.svg' alt='planeta' />
                     <img className='absolute top-0 left-0 z-20 pulse ' style={{ animationDelay: "5s" }} src='./assets/rocket/starts1.svg' alt='estrela' />
